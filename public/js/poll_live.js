c652a4cd3b0e27d819b5ec8934619f57b41b8033
c-delanneau <delanneau.charlie@gmail.com>
2017-05-31 12:05:32 +0200

    Add charts on polls render

--- public/js/poll_live.js
+++ public/js/poll_live.js
@@ -1,4 +1,5 @@
     var socket = io.connect('http://localhost:3000');
+    var charts = {};
 
     socket.on("change_slide", function(slide) {
       var changeSlide=false;
@@ -37,14 +38,32 @@
     function updateSlide(slide) {
       var container = document.getElementById('poll_content');
       $(container).html("LOADER");
+      var chartsData = slide.charts_result;
       var formRenderOpts = {
         container,
         formData: slide.form_json,
-        dataType: 'json'
+        dataType: 'json',
+        onRender: function() {
+          $('.fb-render').find('div').each(function() {
+            $(this).addClass('col-md-12');
+          });
+          console.log(chartsData);
+          $.each(chartsData, function(name, value) {
+            var ctx = document.createElement('canvas');
+            ctx.id = name + '-' + value.type;
+            $(':input[name="' + name + '"]').parents('.form-group').removeClass('col-md-12').addClass('col-md-6').after($('<div class="col-md-6 ' + name + '-chart">' + ctx.outerHTML + '</div>'));
+            ctx = document.getElementById(ctx.id);
+            var myChart = new Chart(ctx, {
+              type: value.type,
+              data: value.data
+            });
+            charts[name] = myChart;
+          });
+        }
       };
 
       $(container).formRender(formRenderOpts);
-    }
+    };
 
     socket.on("last_slide", function(slide) {
       $("#poll_content").html("Merci de votre attention !");
@@ -76,3 +95,35 @@
       //   });
       // }
     });
+
+    socket.on('data_update', function(chartsData) {
+      $.each(charts, function(name, chart) {
+        if (chartsData[name]) {
+          chart.data.datasets[0] = chartsData[name].data.datasets[0];
+          chart.update();
+        }
+      });
+    });
+
+    $('#ok').click(function(){
+      var inputValues = {};
+      $('.form-group').each(function() {
+        $(this).find(':input').each(function() {
+          if ($(this).is(':radio:checked, :checkbox:checked')) {
+            if ($(this).next().is(':input:text'))
+              inputValues[$(this).next().attr('name')] = $(this).next().val();
+            else
+              inputValues[$(this).attr('name')] = $(this).val();
+          }
+          else if (!$(this).is(':radio, :checkbox'))
+            if (!$(this).prev().is(':radio, :checkbox'))
+              inputValues[$(this).attr('name')] = $(this).val();
+          $(this).attr('disabled','disabled');
+        });
+      });
+      socket.emit('input_values', inputValues);
+      $(this).attr('disabled','disabled');
+      $(this).unbind('click');
+      return false;
+    });
+
